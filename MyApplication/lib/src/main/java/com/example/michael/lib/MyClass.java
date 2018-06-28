23e2a465e02f16eb1920c886dac85fd7ca04f0ad
Tyler-Call <40224134+Tyler-Call@users.noreply.github.com>
2018-06-27 20:02:03 -0600

    Starting App

--- MyApplication/lib/src/main/java/com/example/michael/lib/MyClass.java
+++ MyApplication/lib/src/main/java/com/example/michael/lib/MyClass.java
@@ -1,5 +1,9 @@
 package com.example.michael.lib;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * to run, press  ctr + shift + f10
  * in the top right corner, on the dropdown menu that says MyClass with a
@@ -7,62 +11,68 @@ package com.example.michael.lib;
  * "save 'MyClass' Configuration"
  * to run again, click the green arrow
  */
-public class MyClass
-{
-	public static void main(String args[])
-	{
-		System.out.println("Hello, World!");
+public class MyClass {
+    public static void main(String args[]) {
+        System.out.println("Hello, World!");
 
-		//if you don't like my coding style, press Ctr + alt + l
-		// and watch the magic
+        //if you don't like my coding style, press Ctr + alt + l
+        // and watch the magic
 
-		//if you go back to where you saved the configuration
-		//there should be an "Edit Configurations" button
-		//click on that and with MyClass selected, look for
-		//"program arguments" and type in whatever you want
-		//and then click the "OK" button, then the green arrow button
-		if (args.length > 0)
-			System.out.println(isArgPrime(args[0]));
-	}
+        new MyClass().dialog();
+    }
 
-	/**
-	 * change this so we can keep asking it if other numbers are prime
-	 * and exits when "exit" is typed
-	 *
-	 * @param guess
-	 * @return response to the argument as to whether the argument is prime or not
-	 */
-	private static String isArgPrime(String guess)
-	{
-		//we use StringBuilder because they say it's better... or something... efficient-er
-		//don't worry, it's easy to use
-		StringBuilder s = new StringBuilder();
-		s.append("is ");
-		s.append(guess);
-		s.append(" prime?\n");
+    private void dialog() {
+        String input = "";
 
-		//with strings, we can use regular expressions (regex) to see if it isn't a number
-		//just so we can force the use of numbers from user.
-		if (!guess.matches("[0-9]*"))
-		{
-			s.append(guess);
-			s.append(" is not a number\n");
-			return s.toString();
-		}
+        while (!input.equals("end")) {
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            try {
+                System.out.println("give me a number, please");
+                input = br.readLine();
+                System.out.println(isArgPrime(input));
+            } catch (IOException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+    }
 
-		//in Java, (and you can in c++) instantiate your variables where
-		//you use/need them// where it makes sense
-		int potentialPrime = Integer.parseInt(guess);
+    /**
+     * change this so we can keep asking it if other numbers are prime
+     * and exits when "exit" is typed
+     *
+     * @param guess
+     * @return response to the argument as to whether the argument is prime or not
+     */
+    private static String isArgPrime(String guess) {
+        //we use StringBuilder because they say it's better... or something... efficient-er
+        //don't worry, it's easy to use
+        StringBuilder s = new StringBuilder();
+        s.append("is ");
+        s.append(guess);
+        s.append(" prime?\n");
 
-		NewClass newClass = NewClass.get();
+        //with strings, we can use regular expressions (regex) to see if it isn't a number
+        //just so we can force the use of numbers from user.
+        if (!guess.matches("[0-9]*")) {
+            s.append(guess);
+            s.append(" is not a number\n");
+            return s.toString();
+        }
 
-		s.append(potentialPrime);
-		s.append(" is ");
-		if (!newClass.isPrime(potentialPrime))//this is where the magic happens
-			s.append("not ");
-		s.append("prime");
+        //in Java, (and you can in c++) instantiate your variables where
+        //you use/need them// where it makes sense
+        int potentialPrime = Integer.parseInt(guess);
 
-		return s.toString();
-	}
+        Prime prime = Prime.get();
+
+        s.append(potentialPrime);
+        s.append(" is ");
+        if (!prime.isPrime(potentialPrime))//this is where the magic happens
+            s.append("not ");
+        s.append("prime");
+
+        return s.toString();
+    }
 
 }
